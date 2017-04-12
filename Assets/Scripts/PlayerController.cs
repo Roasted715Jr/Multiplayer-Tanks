4f1c636eec503cc2f3010b8e06bceb9f2f0151fc
Matthew Duphily <matthew.duphily@live.com>
2017-04-11 21:36:19 -0400

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+        rb2d.velocity = new Vector2(0, Input.GetAxisRaw("P1 Move") * moveSpeed);
+
+        float playerOneRotation = Input.GetAxisRaw("P1 Turn");
+
+        Quaternion target = Quaternion.FromToRotation(Quaternion.identity, transform.rotation + playerOneRotation);
+        rb2d.transform.rotation = target;
+    }
+}
