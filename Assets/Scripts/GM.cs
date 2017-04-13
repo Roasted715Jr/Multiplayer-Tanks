b482a5b545df379b332b49c1961e00c3775f1b5a
Matthew Duphily <matthew.duphily@live.com>
2017-04-13 07:22:33 -0400

    Established movement and started working on firing

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+    public Transform redStart;
+    public Transform blueStart;
+    public GameObject tankRed;
+    public GameObject tankBlue;
+    public GameObject tank;
+
+	// Use this for initialization
+	void Start () {
+        //Instantiate(tankRed, redStart);
+        //Instantiate(tankBlue, blueStart);
+
+        tankRed = Instantiate(tank, redStart);
+        tankBlue = Instantiate(tank, blueStart);
+        tankRed.GetComponent<PlayerController>().color = "Red";
+        tankBlue.GetComponent<PlayerController>().color = "Blue";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,65 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
+    public float turnSpeed;
+    public float shellSpeed;
+    public string color;
+    //Find a way to index variables to make the index referencing below accurate
+    public Sprite[] sprites;
+    public GameObject barrel;
+    public GameObject shellPrefab;
+    public GameObject barrelEnd;
 
-    private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer barrelSpriteRenderer;
+    private SpriteRenderer shellSpriteRenderer;
+    //Movement and rotation need to be defined up here or else they will only be accessible in the "if/else" statements
+    private float movement;
+    private float rotation;
 
 	// Use this for initialization
 	void Start () {
-        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        barrelSpriteRenderer = barrel.GetComponent<SpriteRenderer>();
+        shellSpriteRenderer = shellPrefab.GetComponent<SpriteRenderer>();
+        
+        
+        if (color == "Red")
+        {
+            spriteRenderer.sprite = sprites[0];
+            barrelSpriteRenderer.sprite = sprites[1];
+            shellSpriteRenderer.sprite = sprites[2];
+        }
+        else if (color == "Blue")
+        {
+            spriteRenderer.sprite = sprites[3];
+            barrelSpriteRenderer.sprite = sprites[4];
+            shellSpriteRenderer.sprite = sprites[5];
+        }
 	}
 	
-	void FixedUpdate () {
-        rb2d.velocity = new Vector2(0, Input.GetAxisRaw("P1 Move") * moveSpeed);
+	void Update () {
+        if (color == "Red")
+        {
+            movement = Input.GetAxisRaw("P1 Move");
+            rotation = Input.GetAxisRaw("P1 Turn");
+        }
+        else if (color == "Blue")
+        {
+            movement = Input.GetAxisRaw("P2 Move");
+            rotation = Input.GetAxisRaw("P2 Turn");
+        }
 
-        float playerOneRotation = Input.GetAxisRaw("P1 Turn");
+        transform.Rotate(Vector3.forward * rotation * -turnSpeed * Time.deltaTime);
+        transform.Translate(Vector3.up * movement * moveSpeed * Time.deltaTime);
 
-        Quaternion target = Quaternion.FromToRotation(Quaternion.identity, transform.rotation + playerOneRotation);
-        rb2d.transform.rotation = target;
+        if (Input.GetKeyDown(KeyCode.Space) && color == "Red" || Input.GetKeyDown(KeyCode.Return) && color == "Blue")
+            Shoot();
+    }
+
+    public void Shoot()
+    {
+        GameObject newShell = Instantiate(shellPrefab, barrelEnd.transform.position, transform.rotation);
+        newShell.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * shellSpeed * Time.deltaTime);
     }
 }
